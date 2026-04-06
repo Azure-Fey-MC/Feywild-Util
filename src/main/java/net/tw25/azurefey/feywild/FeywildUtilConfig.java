c552349b0c14916d44fbc71e70177ab411bcf0f5
Teal Wolf 25 <teal@tw25.net>
2026-04-06 04:07:00 -0400

    Register config

---
+++ src/main/java/net/tw25/azurefey/feywild/FeywildUtilConfig.java
@@ -0,0 +1,19 @@
+package net.tw25.azurefey.feywild;
+
+import com.google.common.collect.Lists;
+import eu.midnightdust.lib.config.MidnightConfig;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeywildUtilConfig extends MidnightConfig {
+    public static final String TEXT = "text";
+    public static final String NUMBERS = "numbers";
+    public static final String SLIDERS = "sliders";
+    public static final String LISTS = "lists";
+    public static final String FILES = "files";
+    public static final String CONDITIONS = "conditions";
+
+    @Entry(category = TEXT) public static boolean compatibilityMode = true;
+}
--- src/main/kotlin/net/tw25/azurefey/feywild/FeywildUtilMod.kt
+++ src/main/kotlin/net/tw25/azurefey/feywild/FeywildUtilMod.kt
@@ -4,28 +4,15 @@ import gay.asoji.fmw.FMW
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import eu.midnightdust.lib.config.MidnightConfig
 
 /**
  * This is your mod's main entrypoint class, this runs on both Client and Server.
  * What you put in here will be loaded on mod initialization time
  */
 object FeywildUtilMod : ModInitializer {
-    /**
-     * This `MOD_ID` value is what your `id` in the fabric.mod.json should be.
-     * Anything that references your mod should call this value/string.
-     */
     val MOD_ID: String = "feywild"
-
-    /**
-     * This logger is used to write text to the console and the log file.
-     * It is considered best practice to use your mod id as the logger's name.
-     * That way, it's clear which mod wrote info, warnings, and errors.
-     */
     val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
-
-    /**
-     * This `MOD_NAME` gets what your mod's user-friendly name is from the `fabric.mod.json`
-     */
     val MOD_NAME: String = FMW.getName(MOD_ID)
 
     /**
@@ -35,5 +22,6 @@ object FeywildUtilMod : ModInitializer {
      */
     override fun onInitialize() {
         LOGGER.info("[${MOD_NAME}] Hello Fabric world from $MOD_NAME/$MOD_ID")
+        MidnightConfig.init("feywild", FeywildUtilConfig::class.java)
     }
 }
